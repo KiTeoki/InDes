0e2bc30e71fb9f119a14a920709a67b150cf3b68
Alice Wenban <wenbanalice@gmail.com>
2018-05-10 15:59:48 +0100

    adding GUI

--- WeatherApp/GUIsettings.java
+++ WeatherApp/GUIsettings.java
@@ -1,13 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class GUIBasic extends JFrame {
+public class GUIsettings {
     JFrame frame = new JFrame("New Frame");
-    public void loadhomeScreen(){
-        frame.setSize(300,400);
+    public void loadSettings() {
+        frame.setSize(300, 400);
         frame.getContentPane().setBackground(Color.cyan);
         frame.setVisible(true);
     }
-
-
 }
