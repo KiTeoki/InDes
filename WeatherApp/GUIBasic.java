2b9e52679c2291f936a9b12a951ed2c488693ec9
Alice Wenban <wenbanalice@gmail.com>
2018-05-10 15:56:53 +0100

    made panel work + blue

--- WeatherApp/GUIBasic.java
+++ WeatherApp/GUIBasic.java
@@ -3,10 +3,10 @@ import java.awt.*;
 
 public class GUIBasic extends JFrame {
     public void loadBackground(){
-        JFrame backgroundFrame = new JFrame();
-        backgroundFrame.setSize(300,400);
-        backgroundFrame.setBackground(Color.cyan);
-        backgroundFrame.setVisible(true);
+        JFrame frame = new JFrame("New Frame");
+        frame.setSize(300,400);
+        frame.getContentPane().setBackground(Color.cyan);
+        frame.setVisible(true);
     }
 
 
