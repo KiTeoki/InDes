5ca76f74334bc5c0467463fabc1c9f6bdd5993b5
Alice Wenban <wenbanalice@gmail.com>
2018-05-10 14:53:24 +0100

    adding a box

--- .idea/WeatherApp/GUIBasic.java
+++ .idea/WeatherApp/GUIBasic.java
@@ -1,5 +1,13 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUIBasic extends JFrame {
-    
+    public void loadBackground(){
+        JFrame backgroundFrame = new JFrame();
+        backgroundFrame.setSize(300,400);
+        backgroundFrame.setBackground(Color.cyan);
+        backgroundFrame.setVisible(true);
+    }
+
+
 }
