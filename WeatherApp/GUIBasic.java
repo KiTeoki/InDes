c4d306fc14ad9f945b494dd1008ad4cba949de9d
Alice Wenban <wenbanalice@gmail.com>
2018-05-11 17:13:05 +0100

    not ready to merge
    trying to work out how to layout

--- WeatherApp/GUIBasic.java
+++ WeatherApp/GUIBasic.java
@@ -5,6 +5,7 @@ public class GUIBasic extends JFrame {
     static JFrame frame = new JFrame("My First Weather App");
     public static JFrame loadhomeScreen(){
         frame.setSize(300,400);
+        frame.setLayout(new GridLayout(3,1));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.cyan);
         return frame;
--- WeatherApp/GUIHome.java
+++ WeatherApp/GUIHome.java
@@ -7,22 +7,40 @@ public class GUIHome {
 
         //tempriture bar goes here
         JPanel tempPan = new JPanel();
+        tempPan.setBackground(Color.cyan);
         JLabel tempLable = new JLabel("Wow this is the Temprture");
-        tempPan.add(tempLable);
+        JPanel settingsBar = new JPanel();
+        settingsBar.setBackground(Color.cyan);
+        settingsBar.setLayout(new GridLayout(1,5));
+        JPanel[] settingspanelHolder = new JPanel[5];
+
+        for(int m = 0; m < 5; m++) {
+                settingspanelHolder[m] = new JPanel();
+                settingsBar.add(settingspanelHolder[m]);
+        }
+
+        JButton settings = new JButton("*");
+        settingspanelHolder[4].add(settings);
+
+        tempPan.add(BorderLayout.NORTH,settingsBar);
+        tempPan.add(BorderLayout.CENTER,tempLable);
 
         //logo pannel
         JPanel logoPan = new JPanel();
+        logoPan.setBackground(Color.cyan);
         JLabel logoLabel = new JLabel("Wow this is the sun its hot");
         logoPan.add(logoLabel);
 
         //this will be the clothes bar
         JPanel clothesPan = new JPanel();
+        clothesPan.setBackground(Color.cyan);
         JLabel clothesLabel = new JLabel("wow flipflops");
         clothesPan.add(clothesLabel);
 
-        base.getContentPane().add(BorderLayout.SOUTH, clothesPan);
-        base.getContentPane().add(BorderLayout.NORTH, tempPan);
-        base.getContentPane().add(BorderLayout.CENTER, logoPan);
+
+        base.getContentPane().add(tempPan);
+        base.getContentPane().add(logoPan);
+        base.getContentPane().add(clothesPan);
         base.setVisible(true);
 
     }
