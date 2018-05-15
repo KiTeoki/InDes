79f9e1680b3ff19c5a6aa57f86e55d2b764f920c
davidwesby <35627483+davidwesby@users.noreply.github.com>
2018-05-15 14:43:01 +0100

    added dropdown and stuff

--- WeatherApp/GUIsettings.java
+++ WeatherApp/GUIsettings.java
@@ -1,20 +1,31 @@
+package WeatherApp;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUIsettings {
-    public static JPanel loadSettings() {
-        JFrame base =GUIBasic.loadhomeScreen();
-        JPanel settingpanel = new JPanel();
-        settingpanel.setLayout(new GridLayout(3,1));
-        settingpanel.setBackground(Color.cyan);
-
+public class GUIsettings
+{
+    public static JPanel loadSettings()
+    {
+        JFrame base = GUIBasic.loadhomeScreen();
+        JPanel settingsPanel = new JPanel();
+        settingsPanel.setLayout(new GridLayout(3,1));
+        settingsPanel.setBackground(Color.cyan);
 
         //location settings bar goes here
         JPanel locationPan = new JPanel();
         locationPan.setBackground(Color.cyan);
         JLabel locLabel = new JLabel("change the location");
+
+        String[] cityStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+
+        JComboBox cityList = new JComboBox(cityStrings);
+//        cityList.setSelectedIndex(4);
+        cityList.addActionListener(new CitySelectionListener());
+
+        locationPan.add(cityList, BorderLayout.CENTER);
 
         JPanel backBar = new JPanel();
         backBar.setBackground(Color.cyan);
@@ -31,7 +42,7 @@ public class GUIsettings {
         JButton backButton = new JButton();
         backButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                base.remove(settingpanel);
+                base.remove(settingsPanel);
                 base.add(GUIHome.loadHome());
                 base.invalidate();
                 base.revalidate();
@@ -48,9 +59,9 @@ public class GUIsettings {
         JLabel logoLabel = new JLabel("change your pin");
         pinPanel.add(logoLabel);
 
-        settingpanel.add(locationPan);
-        settingpanel.add(pinPanel);
+        settingsPanel.add(locationPan);
+        settingsPanel.add(pinPanel);
 
-        return settingpanel;
+        return settingsPanel;
     }
 }
