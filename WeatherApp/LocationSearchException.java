adfc75beb712d2090c355aff6fb630b0c71088a4
Alice Wenban <wenbanalice@gmail.com>
2018-05-17 14:19:35 +0100

    location for uk only

--- WeatherApp/LocationSearchException.java
+++ WeatherApp/LocationSearchException.java
@@ -1,6 +1,9 @@
 package WeatherApp;
 
+import java.util.List;
+
 public class LocationSearchException extends RuntimeException {
-    public Boolean match = false;
-    public LocationSearchException(Boolean matchFound){match=matchFound;}
+    private List matchingCities;
+    public LocationSearchException(List matches){matchingCities = matches;}
+    public List getMatchingCities(){return matchingCities;}
 }
