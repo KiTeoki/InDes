2ae2146fe44042e12cd56aa28cd6c4a5b2f88e6a
Alice Wenban <wenbanalice@gmail.com>
2018-05-17 16:47:30 +0100

    made the screen very slightly bigger
    its now 300/450 looks nicer tbh

--- WeatherApp/GUIBasic.java
+++ WeatherApp/GUIBasic.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class GUIBasic extends JFrame {
     static JFrame frame = new JFrame("My First Weather App");
     public static JFrame loadhomeScreen(){
-        frame.setSize(300,400);
+        frame.setSize(300,450);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.cyan);
