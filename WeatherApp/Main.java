5395fce82bc0295c145ac9e172f011b511bfc5e5
Tudor <tudorcostin7@gmail.com>
2018-05-17 14:44:47 +0100

    needed

--- WeatherApp/Main.java
+++ WeatherApp/Main.java
@@ -7,12 +7,12 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String args[]) {
         try {
-            APIInterface apii = new APIInterface();
-            apii.setLocation("london");
-            ArrayList<ArrayList<WeatherElement>> forecast = apii.getWeather();
-            WeatherElement now = forecast.get(0).get(0);
-            Clothes items[] =WeatherApp.ClothesPickingLogic.whatClothes(now);
-            System.out.println(items[0]+" "+ items[1] +" " + items[2]);
+            //APIInterface apii = new APIInterface();
+            //apii.setLocation("london");
+            //ArrayList<ArrayList<WeatherElement>> forecast = apii.getWeather();
+            //WeatherElement now = forecast.get(0).get(0);
+            //Clothes items[] =WeatherApp.ClothesPickingLogic.whatClothes(now);
+            //System.out.println(items[0]+" "+ items[1] +" " + items[2]);
             JFrame base = GUIBasic.loadhomeScreen();
             base.add(GUIHome.loadHome());
             base.setVisible(true);
@@ -20,5 +20,6 @@ public class Main {
             e.printStackTrace();
         }
     }
+
 
 }
